03a216810a5f5dc61322ef0b73327dfae5c48047
Florian Pfaff <pfaff@ias.uni-stuttgart.de>
2026-05-18 17:52:47 +0200

    Fix alignment-window shim CI

--- src/pymegdec/alignment_window.py
+++ src/pymegdec/alignment_window.py
@@ -6,3 +6,12 @@ from reptrace.decoding.alignment_window import resolved_alignment_window
 from reptrace.decoding.alignment_window import transform_with_alignment_projection
 from reptrace.decoding.alignment_window import uses_separate_alignment_window
 from reptrace.decoding.alignment_window import validate_paired_feature_sets
+
+__all__ = [
+    "AlignmentWindow",
+    "WindowedFeatureSet",
+    "resolved_alignment_window",
+    "transform_with_alignment_projection",
+    "uses_separate_alignment_window",
+    "validate_paired_feature_sets",
+]
